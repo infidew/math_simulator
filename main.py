186b45ad5de94093437f6b173ef441078f36596c
Alexandr Bodnari <infidew666@gmai.com>
2023-04-03 13:45:58 +0100

    added .lower() method

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import time
 from functions import addition, multiplication
 
+self_name = 'Математический симулятор'
+
 
 def main():
-    print('Привет, я Математик, а тебя как зовут?')
+    print(f'Привет, я {self_name}, а тебя как зовут?')
     name = input('Введите имя: ')
     print(f'Приятно познакомиться, {name}.\nХочешь умножать или складывать?')
     game = input('Выберите игру (Умножение/Сложение): ')
-    if game == 'Сложение':
+    if game.lower() == 'сложение':
         range_1 = int(input('Выберите диапазон "до" первого числа: '))
         range_2 = int(input('Выберите диапазон "до" второго числа: '))
         print('Чтобы остановить игру, напиши: Стоп')
@@ -15,7 +17,7 @@ def main():
         print('Поехали!')
         time.sleep(1)
         return addition(range_1, range_2)
-    if game == 'Умножение':
+    if game.lower() == 'умножение':
         range_1 = int(input('Выберите диапазон "до" первого числа: '))
         range_2 = int(input('Выберите диапазон "до" второго числа: '))
         print('Чтобы остановить игру, напиши: Стоп')
